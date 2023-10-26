4dd9782dee40518cc5982be1cdd5543e0da8c182
brandonladen <brandonladen486@gmail.com>
2023-10-26 07:06:00 +0300

    added the source code

---
+++ README.md
@@ -0,0 +1,2 @@
+A user interface of a virtual travel website
+It has a nice Graphical User Interface
---
+++ src/assets/js/try.js
@@ -0,0 +1,12 @@
+let a = 34;
+let b = 21;
+
+let result = a + b;
+
+let weather = "Rainy"
+
+if (weather == "Rainy"){
+    console.log("Carry umbrella");
+} else {
+    console.log("Put on light clothes");
+}
